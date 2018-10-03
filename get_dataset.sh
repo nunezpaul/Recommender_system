2a0f574b1647da59bab224426f5c42d4c664f3c2
nunezpaul <nunez.paul@utexas.edu>
2018-10-03 12:53:40 -0700

    download and setup dataset

---
+++ get_dataset.sh
@@ -0,0 +1,15 @@
+# Modified from https://github.com/jadianes/spark-movie-lens/blob/master/download_dataset.sh
+
+#!/bin/bash
+
+hash wget 2>/dev/null || { echo >&2 "Wget required.  Aborting."; exit 1; }
+hash unzip 2>/dev/null || { echo >&2 "unzip required.  Aborting."; exit 1; }
+
+wget http://files.grouplens.org/datasets/movielens/ml-latest.zip
+unzip -o "ml-latest.zip"
+DESTINATION=~/data/
+[ -d $DESTINATION ] && echo "Directory already exists." || mkdir -p $DESTINATION
+echo "Moving file to ~/data/"
+mv ml-latest $DESTINATION
+echo "Cleaning up..."
+rm "ml-latest.zip"
