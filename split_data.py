17a47455799de03d55e54b0cca02fc38e17d4352
nunezpaul <nunez.paul@utexas.edu>
2018-10-04 11:46:41 -0700

    added option flags to data_splitting

--- split_data.py
+++ split_data.py
@@ -1,8 +1,10 @@
+import argparse
+
 from os.path import expanduser
 from random import random
 
 
-def split_data(filename, test_frac=0.1):
+def split_data(filename, test_frac, skip_count=False):
     # filenames for train and test data
     datafilenames = {}
     datafiles = {}
@@ -29,20 +31,34 @@ def split_data(filename, test_frac=0.1):
             else:
                 datafiles['train'].write(line)
 
-            arr = dict(zip(['user', 'movie', 'rating', 'time'], line.split(',')))
-            for key in vocab_size.keys():
-                vocab_size[key] = max(vocab_size[key], int(arr[key]))
+            if not(skip_count):
+                arr = dict(zip(['user', 'movie', 'rating', 'time'], line.split(',')))
+                for key in vocab_size.keys():
+                    vocab_size[key] = max(vocab_size[key], int(arr[key]))
 
     # Close the files
     for datatype in datatypes:
         datafiles[datatype].close()
 
     # Store the user and movie max vocab
-    for key in vocab_size.keys():
-        with open('{key}_max.csv'.format(key=key), 'w') as f:
-            f.write(str(vocab_size[key]))
+    if not(skip_count):
+        for key in vocab_size.keys():
+            with open('{key}_max.csv'.format(key=key), 'w') as f:
+                f.write(str(vocab_size[key]))
 
 if __name__ == '__main__':
-    # TODO: add flag to change filename/location
+    parser = argparse.ArgumentParser(description='Process data file into a train and test file.')
+    parser.add_argument('--test_frac', help='Set the frac of the data to be set aside for testing.',
+                        default=0.05, type=float)
+    parser.add_argument('--skip_count',
+                        help="""
+                        Add this flag to skip determining max value for users and movies. Enabling this flag speeds 
+                        up the file splitting time considerably. You can do this step manually or run without the flag
+                        once to get the necessary info.
+                        """
+                        '',
+                        action='store_true')
+    args = parser.parse_args()
+
     filename = '{home}/data/ml-latest/ratings.csv'.format(home=expanduser("~"))
-    split_data(filename=filename)
+    split_data(filename=filename, test_frac=args.test_frac, skip_count=args.skip_count)
