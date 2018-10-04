55dde1c06bd9e0e50d9191857f0f7e06d52f59bc
nunezpaul <nunez.paul@utexas.edu>
2018-10-04 15:59:24 -0700

    fixing dataset collection code

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 MovieLens 100K
 
 # Purpose
-Explore how different methods for collaborative filtering compare.
+Explore how different methods for collaborative filtering to compare. Will explore matrix-factorization (MF), MF 
+with regularization, NN model (give u and m what is the probability of rating x), hybrid as well as multitask model.
--- get_dataset.sh
+++ get_dataset.sh
@@ -2,9 +2,11 @@
 
 #!/bin/bash
 
+# Check that the system has all the needed packages
 hash wget 2>/dev/null || { echo >&2 "Wget required.  Aborting."; exit 1; }
 hash unzip 2>/dev/null || { echo >&2 "unzip required.  Aborting."; exit 1; }
 
+# Downloading the data files, extracting and moving them to the desired location
 wget http://files.grouplens.org/datasets/movielens/ml-latest.zip
 unzip -o "ml-latest.zip"
 DESTINATION=~/data/
@@ -13,4 +15,11 @@ echo "Moving file to ~/data/"
 mv ml-latest $DESTINATION
 echo "Cleaning up..."
 rm "ml-latest.zip"
+
+# Splitting the data into training and testing data
+ORIGINAL=`pwd`
+cd $BASH_SOURCE
+echo Splitting the data into training and testing files...
 python split_data.py
+echo Done!
+cd $ORIGINAL
