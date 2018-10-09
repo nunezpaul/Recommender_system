4c1f9963cc43974ba0eca458ed878abdd957c1b0
nunezpaul <nunez.paul@utexas.edu>
2018-10-08 19:19:06 -0700

    bug fixing

--- get_dataset.sh
+++ get_dataset.sh
@@ -5,22 +5,23 @@
 hash wget 2>/dev/null || { echo >&2 "Wget required.  Aborting."; exit 1; }
 hash unzip 2>/dev/null || { echo >&2 "unzip required.  Aborting."; exit 1; }
 
+# Moving to the same directory as this script
+ORIGINAL=`pwd`
+ROOT= `dirname $BASH_SOURCE`
+cd ROOT
+
 # Downloading the data files, extracting and moving them to the desired location
-# TODO: need to refactor such that the data files are contained within the same directory.
 wget http://files.grouplens.org/datasets/movielens/ml-latest.zip
 unzip -o "ml-latest.zip"
 
 # Location where to place the data
-ROOT= `dirname $BASH_SOURCE`
-[ -d $ROOT/data ] && echo "Directory already exists." || (mkdir -p $ROOT && echo "Making new directory")
+[ -d data ] && echo "Directory already exists." || (mkdir -p data && echo "Making new directory")
 echo "Moving file to $ROOT/data"
-mv ml-latest $ROOT/data
+mv ml-latest data
 echo "Cleaning up..."
 rm "ml-latest.zip"
 
 # Splitting the data into training and testing data
-ORIGINAL=`pwd`
-cd $ROOT  # Gets the containing directory of the bash script
 echo Splitting the data into training and testing files...
 python split_data.py && echo "Done with train/test split!" || echo "FAILED!"
 
@@ -29,9 +30,9 @@ echo Sharding trainining files..
 python shard_training_data.py && echo "Sharding complete! Cleaning up..." || echo "FAILED!"
 
 # Cleaning up data file directory
-SHARD_ROOT=$ROOT/data/ml-latest/shards
-[ -d SHARD_ROOT ] && echo "Directory already exists." || (mkdir -p $ROOT && echo "Making new directory.")
-mv $ROOT/data/ml-latest/train_* $SHARD_ROOT && echo "Done cleaning. Shards moved to $SHARD_ROOT" || echo "FAILED!"
+SHARD_ROOT=/data/ml-latest/shards
+[ -d SHARD_ROOT ] && echo "Directory already exists." || (mkdir -p $SHARD_ROOT && echo "Making new directory.")
+mv data/ml-latest/train_* $SHARD_ROOT && echo "Done cleaning. Shards moved to $SHARD_ROOT" || echo "FAILED!"
 
 # Returning to original directory
 cd $ORIGINAL
